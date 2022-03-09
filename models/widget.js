b559914c28e9ad6d235daca0bfaafeee24b6719d
harrdev <jasonharrphx@live.com>
2022-03-09 10:21:50 -0700

    added widget model

---
+++ models/widget.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const WidgetsSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: false,
+		},
+        quantity: {
+			type: Number,
+			required: false,
+		},
+        cost: {
+			type: String,
+			required: false,
+		},
+        manufacturer: {
+			type: String,
+			required: false,
+		},
+        notes: {
+            type: String,
+            required: false,
+        },
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Widgets', WidgetsSchema)
